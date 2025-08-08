1fd6a5911394010804531535243b6b857facacb1
gabrielprimieri <gabriel_primieri@hotmail.com>
2025-08-08 14:28:03 -0300

    Ajustando a DepositoService

--- banco/src/main/kotlin/com/banco/nova/era/service/DepositoService.kt
+++ banco/src/main/kotlin/com/banco/nova/era/service/DepositoService.kt
@@ -2,10 +2,13 @@ package com.banco.nova.era.service
 
 import com.banco.nova.era.dto.DepositoDTO
 import com.banco.nova.era.repository.UsuarioRepository
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
 class DepositoService(private val usuarioRepository: UsuarioRepository) {
+
+    @Transactional
     fun verificar(payload: DepositoDTO): Boolean{
         val usuario = usuarioRepository.findById(payload.cpf).orElse(null)
 
